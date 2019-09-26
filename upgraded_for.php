83ede41fecde963bebd87e5a22d0274a5aa6160e
Randy Brouwer <2105070@talnet.nl>
2019-09-26 08:56:08 +0100

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,13 @@
+<?php
+
+$people = array(
+    "Sjonnie de Wiel",
+    "Herman Kaal",
+    "Henk de Steen",
+    "Inge Kerkhoven",
+    "Gert Kruiswijk"
+);
+
+foreach ($people as $yeet) {
+    echo "$yeet\n";
+}
